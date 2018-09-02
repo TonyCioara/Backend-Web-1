c08539f6edabb419321d2264666de4ded78082e1
TonyCioara <tonyangelo9707@gmail.com>
2018-09-01 20:39:40 -0700

    Added mongodb and mongodb and Review model

--- rotten-potatoes/app.js
+++ rotten-potatoes/app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+
+const Review = mongoose.model('Review', {
+    title: String
+});
+
 var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Hello World!'});
+    Review.find()
+        .then(reviews => {
+            res.render('reviews-index', {reviews: reviews});
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 app.listen(3000, () => {
