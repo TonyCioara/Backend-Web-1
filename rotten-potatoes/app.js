bd9b377e2b6c92e62af304726ca000293ae4ce28
TonyCioara <tonyangelo9707@gmail.com>
2018-09-02 10:45:14 -0700

    Users can see individual reviews

--- rotten-potatoes/app.js
+++ rotten-potatoes/app.js
@@ -6,8 +6,9 @@ mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true }
 
 const Review = mongoose.model('Review', {
     title: String,
-    descriptors: String,
-    movieTitle: String
+    description: String,
+    reviewTitle: String,
+    rating: Number
 });
 
 const bodyParser = require('body-parser');
@@ -21,25 +22,34 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.get('/', (req, res) => {
     Review.find()
         .then(reviews => {
+            console.log(reviews)
             res.render('reviews-index', {reviews: reviews});
         })
         .catch(err => {
-            console.log(err);
+            console.log('ERROR: ', err);
         });
 });
 
 app.get("/reviews/new", (req, res) => {
     res.render('reviews-new', {});
-})
+});
+
+app.get('/reviews/:id', (req, res) => {
+    Review.findById(req.params.id).then((review) => {
+        res.render('reviews-show', { review: review})
+    }).catch((err) => {
+        console.log(err.message);
+    });
+});
 
 app.post('/reviews', (req, res) => {
     Review.create(req.body).then((review) => {
         console.log(review);
-        res.redirect('/');
+        res.redirect(`/reviews/${review._id}`);
     }).catch((err) => {
         console.log(err.message);
-    })
-})
+    });
+});
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
