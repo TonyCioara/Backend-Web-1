0e0eed65349da99bdb4ea452b8c21da71d23e7d4
TonyCioara <tonyangelo9707@gmail.com>
2018-09-04 15:12:31 -0700

    Users can delete reviews

--- rotten-potatoes/app.js
+++ rotten-potatoes/app.js
@@ -13,34 +13,42 @@ const Review = mongoose.model('Review', {
 
 const bodyParser = require('body-parser');
 
+const methodOverride = require('method-override');
+
 var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     Review.find()
         .then(reviews => {
-            console.log(reviews)
             res.render('reviews-index', {reviews: reviews});
         })
         .catch(err => {
             console.log('ERROR: ', err);
-        });
-});
+        })
+})
 
 app.get("/reviews/new", (req, res) => {
     res.render('reviews-new', {});
-});
+})
 
 app.get('/reviews/:id', (req, res) => {
     Review.findById(req.params.id).then((review) => {
         res.render('reviews-show', { review: review})
     }).catch((err) => {
         console.log(err.message);
-    });
-});
+    })
+})
+
+app.get('/reviews/:id/edit', function (req, res) {
+    Review.findById(req.params.id, function(err, review) {
+        res.render('reviews-edit', {review: review});
+    })
+})
 
 app.post('/reviews', (req, res) => {
     Review.create(req.body).then((review) => {
@@ -48,9 +56,29 @@ app.post('/reviews', (req, res) => {
         res.redirect(`/reviews/${review._id}`);
     }).catch((err) => {
         console.log(err.message);
-    });
-});
+    })
+})
+
+app.put('/reviews/:id/', (req, res) => {
+    Review.findByIdAndUpdate(req.params.id, req.body)
+        .then(review => {
+            res.redirect(`/reviews/${review._id}`);
+        })
+        .catch(err => {
+             console.log(err.message)
+        })
+})
+
+app.delete('/reviews/:id', function (req, res) {
+    Review.findByIdAndRemove(req.params.id)
+        .then((review) => {
+            res.redirect('/');
+        }).catch((err) => {
+            console.log(err.message);
+    })
+})
+
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
-});
+})
